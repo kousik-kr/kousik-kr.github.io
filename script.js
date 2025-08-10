468bbd7c003d0bb08d7cdc83f4b17d9b3a46577b
kousik-kr <105310962+kousik-kr@users.noreply.github.com>
2025-08-11 00:51:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -75,3 +75,75 @@ if (slideshow) {
     setInterval(changeBackground, 5000); // every 5 sec
 }
 
+const url = 'My_CV.pdf';
+let pdfDoc = null,
+    pageNum = 1,
+    pageRendering = false,
+    pageNumPending = null,
+    scale = 1.0,
+    canvas = document.getElementById('resume-canvas'),
+    ctx = canvas.getContext('2d');
+
+pdfjsLib.getDocument(url).promise.then(function (pdfDoc_) {
+    pdfDoc = pdfDoc_;
+    document.getElementById('page_count').textContent = pdfDoc.numPages;
+    renderPage(pageNum);
+});
+
+function renderPage(num) {
+    pageRendering = true;
+    pdfDoc.getPage(num).then(function (page) {
+        let viewport = page.getViewport({ scale: scale });
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+
+        let renderContext = {
+            canvasContext: ctx,
+            viewport: viewport
+        };
+        let renderTask = page.render(renderContext);
+
+        renderTask.promise.then(function () {
+            pageRendering = false;
+            if (pageNumPending !== null) {
+                renderPage(pageNumPending);
+                pageNumPending = null;
+            }
+        });
+    });
+
+    document.getElementById('page_num').textContent = num;
+}
+
+function queueRenderPage(num) {
+    if (pageRendering) {
+        pageNumPending = num;
+    } else {
+        renderPage(num);
+    }
+}
+
+document.getElementById('prev').addEventListener('click', function () {
+    if (pageNum <= 1) return;
+    pageNum--;
+    queueRenderPage(pageNum);
+});
+
+document.getElementById('next').addEventListener('click', function () {
+    if (pageNum >= pdfDoc.numPages) return;
+    pageNum++;
+    queueRenderPage(pageNum);
+});
+
+document.getElementById('zoom_in').addEventListener('click', function () {
+    scale += 0.2;
+    queueRenderPage(pageNum);
+});
+
+document.getElementById('zoom_out').addEventListener('click', function () {
+    if (scale > 0.4) {
+        scale -= 0.2;
+        queueRenderPage(pageNum);
+    }
+});
+
